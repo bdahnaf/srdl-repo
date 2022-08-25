58895a040b637651e10bbf9e484af7112f6da227
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-08-26 00:51:03 +0600

    Add Iamge Model

---
+++ Models/Image.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace srdl_repo.Models
+{
+    public class Image
+    {
+        [Key]
+        public int ImageId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public string Extension { get; set; }
+        public DateTime PostedDate { get; set; }
+    }
+}
