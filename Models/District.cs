6843e96679bc6abd939a27c38424df4cdf3d457a
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-08-25 23:10:28 +0600

    Add District Model

---
+++ Models/District.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace srdl_repo.Models
+{
+    public class District
+    {
+        [Key]
+        public int DistrictId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
