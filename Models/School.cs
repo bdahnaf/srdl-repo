29b74c0c28643ee01beef8100395b237d01e9a22
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-08-26 00:49:41 +0600

    Add School Model

---
+++ Models/School.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace srdl_repo.Models
+{
+    public class School
+    {
+        [Key]
+        public int SchoolId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string NameInBangla { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
