068091d4e0fd8d3d3c6517f9cd3e59a71b6ed67b
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-08-26 00:14:52 +0600

    Add Upazila Model

---
+++ Models/Upazila.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace srdl_repo.Models
+{
+    public class Upazila
+    {
+        [Key]
+        public string UpzailaId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
