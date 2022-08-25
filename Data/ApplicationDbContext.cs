62155f53c2afa4df90ed0f3cc1c8ed1f6d2b1d70
Ahnaf Shahriar <bdahnaf@gmail.com>
2022-08-26 00:33:20 +0600

    Add DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using srdl_repo.Models;
+
+namespace srdl_repo.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Division> Divisions { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<Upazila> Upazilas { get; set; }
+    }
+}
